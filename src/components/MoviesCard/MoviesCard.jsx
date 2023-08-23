432aeec3842f61929bd5e614a2b1b61e1c0c37a0
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-23 13:51:28 +0300

    Тест

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -47,7 +47,7 @@ export default function MoviesCard({ movie, pageLocation, isSaved }) {
   return (
     <article className='movie'>
       <img
-        src={movie.image.formats.url}
+        src={isSaved ? movie.image : `https://api.nomoreparties.co/${movie.image.url}`}
         alt={movie.name}
         className='movie__image'
       />
