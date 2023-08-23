312f7aea5ea688ec9a7f3a053fc80ab2e93f8bf4
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-23 16:16:51 +0300

    Тест

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,53 +1,66 @@
 import '../MoviesCardList/MoviesCardList.css'
 /* import { movies } from '../../utils/movies'; */
 import MoviesCard from '../MoviesCard/MoviesCard';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useLocation } from 'react';
 
 export default function MoviesCardList({ pageLocation, movies, savedMovies }) {
+  const { pathname } = useLocation();
 
-    const [cardsToShow, setCardsToShow] = useState(4); // Начальное количество карточек
-  
-    useEffect(() => {
-      const handleResize = () => {
-        let newCardsToShow;
-  
-        if (window.innerWidth >= 1280) {
-          newCardsToShow = 12;
-        } else if (window.innerWidth >= 768) {
-          newCardsToShow = 8;
-        } else {
-          newCardsToShow = 5;
-        }
-  
-        setCardsToShow(newCardsToShow);
-      };
-  
-      const throttledResize = () => {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(handleResize, 250); // Откладываем вызов на 250 мс
-      };
-  
-      let resizeTimer;
-      window.addEventListener("resize", throttledResize);
-      handleResize(); // Инициализация значения при первой загрузке
-  
-      return () => {
-        window.removeEventListener("resize", throttledResize);
-      };
-    }, []);
-  
-    const loadMoreCards = () => {
+  const [cardsToShow, setCardsToShow] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      let newCardsToShow;
+
       if (window.innerWidth >= 1280) {
-        setCardsToShow(prevCards => prevCards + 12);
+        newCardsToShow = 12;
       } else if (window.innerWidth >= 768) {
-        setCardsToShow(prevCards => prevCards + 8);
+        newCardsToShow = 8;
       } else {
-        setCardsToShow(prevCards => prevCards + 5);
+        newCardsToShow = 5;
       }
+
+      setCardsToShow(newCardsToShow);
     };
-  
-    return (
-      <section className="movies-card-list">
+
+    const throttledResize = () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(handleResize, 250);
+    };
+
+    let resizeTimer;
+    window.addEventListener("resize", throttledResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", throttledResize);
+    };
+  }, []);
+
+  const loadMoreCards = () => {
+    if (window.innerWidth >= 1280) {
+      setCardsToShow(prevCards => prevCards + 12);
+    } else if (window.innerWidth >= 768) {
+      setCardsToShow(prevCards => prevCards + 8);
+    } else {
+      setCardsToShow(prevCards => prevCards + 5);
+    }
+  };
+
+  return (
+    <section className="movies-card-list">
+      {pathname === '/saved-movies' ? (
+        <ul className="movies-card-list__section">
+          {movies.slice(0, cardsToShow).map((movie) => (
+            <MoviesCard
+              key={movie.id || movie._id}
+              movie={movie}
+              pageLocation={pageLocation}
+              /* saved={getSavedMovieCard(savedMovies, movie)} */
+              isSaved={movie.isSaved}
+            />
+          ))}
+        </ul>) : (
         <ul className="movies-card-list__section">
           {movies.slice(0, cardsToShow).map((movie) => (
             <MoviesCard
@@ -59,71 +72,72 @@ export default function MoviesCardList({ pageLocation, movies, savedMovies }) {
             />
           ))}
         </ul>
-        {cardsToShow < movies.length ? (
-          <button
-            className="movies-card-list__load-more"
-            onClick={loadMoreCards}
-          >
-            Ещё
-          </button>
-        ) : null}
-      </section>
-    );
-  };
+      )}
+      {cardsToShow < movies.length ? (
+        <button
+          className="movies-card-list__load-more"
+          onClick={loadMoreCards}
+        >
+          Ещё
+        </button>
+      ) : null}
+    </section>
+  );
+};
 
-  /* const [shownMovies, setShownMovies] = useState(0);
+/* const [shownMovies, setShownMovies] = useState(0);
 
-  function shownCount() {
-    const display = window.innerWidth;
-    if (display > 1180) {
-      setShownMovies(16);
-    } else if (display > 1023) {
-      setShownMovies(12);
-    } else if (display > 800) {
-      setShownMovies(8);
-    } else if (display < 800) {
-      setShownMovies(5);
-    }
+function shownCount() {
+  const display = window.innerWidth;
+  if (display > 1180) {
+    setShownMovies(16);
+  } else if (display > 1023) {
+    setShownMovies(12);
+  } else if (display > 800) {
+    setShownMovies(8);
+  } else if (display < 800) {
+    setShownMovies(5);
   }
+}
 
-  useEffect(() => {
-    shownCount();
-  }, []);
+useEffect(() => {
+  shownCount();
+}, []);
 
-  function showMore() {
-    const display = window.innerWidth;
-    if (display > 1180) {
-      setShownMovies(shownMovies + 4);
-    } else if (display > 1023) {
-      setShownMovies(shownMovies + 3);
-    }
-    else if (display < 1023) {
-      setShownMovies(shownMovies + 2);
-    }
+function showMore() {
+  const display = window.innerWidth;
+  if (display > 1180) {
+    setShownMovies(shownMovies + 4);
+  } else if (display > 1023) {
+    setShownMovies(shownMovies + 3);
+  }
+  else if (display < 1023) {
+    setShownMovies(shownMovies + 2);
+  }
+} */
+
+/*   function getSavedMovieCard(savedMovies, movie) {
+    return savedMovies.find((savedMovie) => savedMovie.movieId === movie.id);
   } */
 
-  /*   function getSavedMovieCard(savedMovies, movie) {
-      return savedMovies.find((savedMovie) => savedMovie.movieId === movie.id);
-    } */
-
- /*  return (
-    <section className="movies-card-list">
-      <ul className="movies-card-list__section">
-        {movies.map((movie) => (
-          <MoviesCard
-            key={movie.id || movie._id}
-            movie={movie}
-            pageLocation={pageLocation}
-             saved={getSavedMovieCard(savedMovies, movie)} 
-            isSaved={movie.isSaved}
-          />))}
-      </ul>
-      {movies.length > shownMovies ? (
-      <button
-        className="movies-card-list__load-more"
-        onClick={showMore}
-      >
-        Ещё
-      </button>) : ('')}
-    </section>
-  ); */
+/*  return (
+   <section className="movies-card-list">
+     <ul className="movies-card-list__section">
+       {movies.map((movie) => (
+         <MoviesCard
+           key={movie.id || movie._id}
+           movie={movie}
+           pageLocation={pageLocation}
+            saved={getSavedMovieCard(savedMovies, movie)} 
+           isSaved={movie.isSaved}
+         />))}
+     </ul>
+     {movies.length > shownMovies ? (
+     <button
+       className="movies-card-list__load-more"
+       onClick={showMore}
+     >
+       Ещё
+     </button>) : ('')}
+   </section>
+ ); */
