c1de343bc6abeb170aa37fd25fdc98a53edc89c5
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-27 16:17:33 +0300

    Тест

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,12 +1,78 @@
 import { useLocation } from 'react-router-dom';
+import '../MoviesCard/MoviesCard.css';
+
+export default function MoviesCard({ movie, isSaved, onCardSave, onCardDelete, saved }) {
+  const location = useLocation().pathname;
+  const pageLocation = location === '/movies';
+
+  const convertMinutesToHours = (minutes) => {
+    if (isNaN(minutes) || minutes < 0) {
+      return 'Некорректное значение';
+    }
+
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+
+    return `${hours}ч ${remainingMinutes}м`;
+  };
+
+  const handleSaveClick = () => {
+    if (isSaved) {
+      onCardDelete(movie);
+    } else {
+      onCardSave(movie);
+    }
+  };
+
+  const renderButton = () => {
+    if (pageLocation) {
+      if (isSaved) {
+        return (
+          <button
+            onClick={handleSaveClick}
+            type='button'
+            className='movie__button-delete'
+          />
+        );
+      } else {
+        return (
+          <button
+            onClick={handleSaveClick}
+            type='button'
+            className={`movie__button-save ${isSaved ? 'movie__button-saved' : ''}`}
+          >
+            {isSaved ? '' : 'Сохранить'}
+          </button>
+        );
+      }
+    }
+  };
+
+  return (
+    <article className='movie'>
+      <img
+        src={isSaved ? movie.image : `https://api.nomoreparties.co/${movie.image.url}`}
+        alt={movie.name}
+        className='movie__image'
+      />
+      <div className='movie__description'>
+        <div className='movie__container'>
+          <h2 className='movie__name'>{movie.nameRU}</h2>
+          <span className='movie__duration'>{convertMinutesToHours(movie.duration)}</span>
+        </div>
+        {renderButton()}
+      </div>
+    </article>
+  );
+}
+
+
+/* import { useLocation } from 'react-router-dom';
 import '../MoviesCard/MoviesCard.css'
 
 
 export default function MoviesCard({ movie, isSaved, onCardSave, onCardDelete, saved }) {
-  /*  const currentLocation = useLocation();
- 
-   const handleBookmarkClick = () => onBookmark(movie);
-   const handleDeleteClick = () => onDelete(movie); */
+
   const location = useLocation().pathname;
 
   const pageLocation = location === '/movies';
@@ -29,6 +95,7 @@ export default function MoviesCard({ movie, isSaved, onCardSave, onCardDelete, s
 
     return `${hours}ч ${remainingMinutes}м`;
   }
+  
   const buttonType = () => {
     if (pageLocation && !isSaved) {
       return (
@@ -75,4 +142,4 @@ export default function MoviesCard({ movie, isSaved, onCardSave, onCardDelete, s
       </div>
     </article>
   )
-}
+} */
