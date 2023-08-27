7fd06f7ab9ded5afc1f4c40659202949fa0a2eed
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-27 18:33:31 +0300

    Тест

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,7 +1,7 @@
-import { useState, useCallback  } from "react";
+import { useState, useCallback } from "react";
 
 //хук управления формой и валидации формы
-export default function useFormWithValidation() {
+export default function Validation() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
@@ -10,8 +10,8 @@ export default function useFormWithValidation() {
     const target = event.target;
     const name = target.name;
     const value = target.value;
-    setValues({...values, [name]: value});
-    setErrors({...errors, [name]: target.validationMessage });
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: target.validationMessage });
     setIsValid(target.closest("form").checkValidity());
   };
 
@@ -26,47 +26,3 @@ export default function useFormWithValidation() {
 
   return { values, setValues, handleChange, errors, isValid, resetForm };
 }
-
-/* import { useState, useCallback } from 'react';
-
-const Validation = () => {
-  const [enteredValues, setEnteredValues] = useState({});
-  const [errors, setErrors] = useState({});
-  const [isFormValid, setIsFormValid] = useState(false);
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    setEnteredValues({
-      ...enteredValues,
-      [name]: value,
-    });
-
-    setErrors({
-      ...errors,
-      [name]: event.target.validationMessage,
-    });
-
-    setIsFormValid(event.target.closest('#form').checkValidity());
-  };
-
-  const resetForm = useCallback(
-    (newValues = {}, newErrors = {}, newIsFormValid = false) => {
-      setEnteredValues(newValues);
-      setErrors(newErrors);
-      setIsFormValid(newIsFormValid);
-    },
-    [setEnteredValues, setErrors, setIsFormValid],
-  );
-
-  return {
-    enteredValues,
-    errors,
-    handleChange,
-    isFormValid,
-    resetForm,
-  };
-};
-
-export default Validation; */
