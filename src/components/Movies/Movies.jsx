f9c2d56628b44eaa4baf2fb21baa608fe321dcf8
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-29 15:01:44 +0300

    Тест

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -4,9 +4,31 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import '../Movies/Movies.css';
 
 export default function Movies({ movies, savedMoviesList, onCardSave, onCardDelete }) {
+
+  function filterMovies(movies, query) {
+    const filteredMovies = movies.filter((movie) => {
+      const lowerCaseQuery = query.toLowerCase();
+      const nameRULowerCase = movie.nameRU.toLowerCase();
+      const nameENLowerCase = movie.nameEN.toLowerCase();
+  
+      return (
+        (nameRULowerCase.includes(lowerCaseQuery) ||
+         nameENLowerCase.includes(lowerCaseQuery)) &&
+        ( movie.duration <= 40)
+      );
+    });
+
+  
+    return filteredMovies;
+  }
+
+
   return (
     <main className="main">
-      <SearchForm />
+      <SearchForm
+        onSearchMovies={filterMovies}
+        movies={movies}
+      />
       {/*   <Preloader /> */}
       <MoviesCardList
         movies={movies}
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,31 +1,46 @@
+import { useState } from 'react';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import '../SearchForm/SearchForm.css'
 
-export default function SearchForm() {
-  return(
+export default function SearchForm({ onSearchMovies, movies }) {
+  const [query, setQuery] = useState('');
+
+  function handleChangeSearch(e) {
+    setQuery(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSearchMovies(movies, query);
+  }
+
+  return (
     <section className='search'>
-    <div className='search__container'>
-      <form
-        noValidate
-        className='search__form'
-        name='search'
-      >
-        <input
-          className='search__input'
+      <div className='search__container'>
+        <form
+          noValidate
+          className='search__form'
           name='search'
-          type='text'
-          placeholder='Фильм'
-          autoComplete='off'
-          required
+          onSubmit={handleSubmit}
+        >
+          <input
+            className='search__input'
+            name='search'
+            type='text'
+            placeholder='Фильм'
+            autoComplete='off'
+            onChange={handleChangeSearch}
+            value={query || ''}
+            required
+          />
+          <button
+            className='search__button'
+            type='submit'
+          ></button>
+        </form>
+        <FilterCheckbox
         />
-        <button
-          className='search__button'
-          type='submit'
-        ></button>
-      </form>
-      <FilterCheckbox
-      />
-    </div>
-  </section>
+      </div>
+    </section>
   );
 };
