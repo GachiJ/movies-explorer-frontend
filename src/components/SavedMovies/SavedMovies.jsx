d0f4cc4d39a3934f0cb0a9ce27e755c64e1cead0
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-30 19:49:56 +0300

    Тест

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import '../SavedMovies/SavedMovies.css';
 import SearchForm from '../SearchForm/SearchForm';
 
 export default function SavedMovies({ movies, savedMoviesList, onCardSave, onCardDelete }) {
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
+  const onSearch = (filteredMovies) => {
+    setFilteredMovies(filteredMovies);
+  };
+
   return (
     <main className="main">
-      <SearchForm />
+      <SearchForm
+        savedMoviesList={savedMoviesList}
+        onSearch={onSearch}
+      />
       {/*   <Preloader /> */}
       <MoviesCardList
         movies={movies}
-        savedMoviesList={savedMoviesList}
+        savedMoviesList={filteredMovies.length > 0 ? filteredMovies : savedMoviesList}
         onCardSave={onCardSave}
         onCardDelete={onCardDelete}
         isSaved={true}
