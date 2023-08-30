41ff7ad2038b327e31c44c9766451ea7f0fc11ce
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-30 12:30:44 +0300

    Тест

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,17 +2,25 @@ import SearchForm from '../SearchForm/SearchForm';
 /* import Preloader from '../Preloader/Preloader'; */
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import '../Movies/Movies.css';
+import { useState } from 'react';
 
 export default function Movies({ movies, savedMoviesList, onCardSave, onCardDelete }) {
+  const [filteredMovies, setFilteredMovies] = useState([]); // Добавьте состояние для отфильтрованных фильмов
+
+  // Функция для обновления отфильтрованных фильмов
+  const handleSearch = (filteredMovies) => {
+    setFilteredMovies(filteredMovies);
+  };
   
   return (
     <main className="main">
       <SearchForm
         movies={movies}
+        onSearch={handleSearch}
       />
       {/*   <Preloader /> */}
       <MoviesCardList
-        movies={movies}
+        movies={filteredMovies.length > 0 ? filteredMovies : movies}
         savedMoviesList={savedMoviesList}
         onCardSave={onCardSave}
         onCardDelete={onCardDelete}
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,63 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+
+export default function SearchForm({ movies, savedMoviesList, onSearch }) {
+  const [query, setQuery] = useState('');
+  const { pathname } = useLocation();
+
+  function handleChangeSearch(e) {
+    setQuery(e.target.value);
+  }
+
+  function filterMovies(e) {
+    e.preventDefault();
+    const moviesToFilter = pathname === '/movies' ? movies : savedMoviesList;
+    const filteredMovies = moviesToFilter.filter((movie) => {
+      const lowerCaseQuery = query.toLowerCase();
+      const nameRULowerCase = movie.nameRU.toLowerCase();
+      const nameENLowerCase = movie.nameEN.toLowerCase();
+
+      return (
+        (nameRULowerCase.includes(lowerCaseQuery) ||
+          nameENLowerCase.includes(lowerCaseQuery)) &&
+        (movie.duration <= 40)
+      );
+    });
+
+    onSearch(filteredMovies);
+  }
+
+  return (
+    <section className='search'>
+      <div className='search__container'>
+        <form
+          noValidate
+          className='search__form'
+          name='search'
+          onSubmit={filterMovies}
+        >
+          <input
+            className='search__input'
+            name='search'
+            type='text'
+            placeholder='Фильм'
+            autoComplete='off'
+            onChange={handleChangeSearch}
+            value={query || ''}
+            required
+          />
+          <button
+            className='search__button'
+            type='submit'
+          ></button>
+        </form>
+        <FilterCheckbox />
+      </div>
+    </section>
+  );
+}
+/* import { useEffect, useState } from 'react';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import '../SearchForm/SearchForm.css'
 import { useLocation } from 'react-router-dom';
@@ -15,7 +74,7 @@ export default function SearchForm({ movies, savedMoviesList }) {
   function filterMovies(e) {
     e.preventDefault();
     const moviesToFilter = pathname === '/movies' ? movies : savedMoviesList;
-    const filteredMovies = moviesToFilter.filter((movie) => {
+    filteredMovies = moviesToFilter.filter((movie) => {
       const lowerCaseQuery = query.toLowerCase();
       const nameRULowerCase = movie.nameRU.toLowerCase();
       const nameENLowerCase = movie.nameEN.toLowerCase();
@@ -61,4 +120,4 @@ export default function SearchForm({ movies, savedMoviesList }) {
       </div>
     </section>
   );
-};
+}; */
