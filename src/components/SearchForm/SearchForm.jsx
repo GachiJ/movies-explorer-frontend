ec720162e46107d57982cf3a40d480cb781407c7
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-30 20:54:23 +0300

    Функциональность на «React» и JS

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -6,7 +6,7 @@ import '../SearchForm/SearchForm.css'
 export default function SearchForm({ movies, savedMoviesList, onSearch }) {
   const [query, setQuery] = useState('');
   const { pathname } = useLocation();
-  const [shortMovies, setShortMovies] = useState(false); 
+  const [shortMovies, setShortMovies] = useState(false);
 
   function handleChangeSearch(e) {
     setQuery(e.target.value);
@@ -56,72 +56,8 @@ export default function SearchForm({ movies, savedMoviesList, onSearch }) {
           ></button>
         </form>
         <FilterCheckbox handleShortFilms={() => setShortMovies(!shortMovies)} // Инвертируем состояние короткометражных фильмов
-          shortMovies={shortMovies}/>
+          shortMovies={shortMovies} />
       </div>
     </section>
   );
 }
-/* import { useEffect, useState } from 'react';
-import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import '../SearchForm/SearchForm.css'
-import { useLocation } from 'react-router-dom';
-
-export default function SearchForm({ movies, savedMoviesList }) {
-  const [query, setQuery] = useState('');
-  const { pathname } = useLocation();
-  const [filteredMovies, setFilteredMovies] = useState([]);
-
-  function handleChangeSearch(e) {
-    setQuery(e.target.value);
-  }
-
-  function filterMovies(e) {
-    e.preventDefault();
-    const moviesToFilter = pathname === '/movies' ? movies : savedMoviesList;
-    filteredMovies = moviesToFilter.filter((movie) => {
-      const lowerCaseQuery = query.toLowerCase();
-      const nameRULowerCase = movie.nameRU.toLowerCase();
-      const nameENLowerCase = movie.nameEN.toLowerCase();
-
-      return (
-        (nameRULowerCase.includes(lowerCaseQuery) ||
-          nameENLowerCase.includes(lowerCaseQuery)) &&
-        (movie.duration <= 40)
-      );
-    });
-
-    setFilteredMovies(filteredMovies);
-  }
-
-
-  return (
-    <section className='search'>
-
-      <div className='search__container'>
-        <form
-          noValidate
-          className='search__form'
-          name='search'
-          onSubmit={filterMovies}
-        >
-          <input
-            className='search__input'
-            name='search'
-            type='text'
-            placeholder='Фильм'
-            autoComplete='off'
-            onChange={handleChangeSearch}
-            value={query || ''}
-            required
-          />
-          <button
-            className='search__button'
-            type='submit'
-          ></button>
-        </form>
-        <FilterCheckbox
-        />
-      </div>
-    </section>
-  );
-}; */
