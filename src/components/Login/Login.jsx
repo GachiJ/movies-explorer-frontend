2bfa428c847d886ec54d82e738c635cbfbcc4ff7
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-31 17:34:28 +0300

    Тест

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,22 +2,32 @@ import '../Login/Login.css'
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from '../../images/logo.svg'
+import Validation from '../../utils/Validation';
 
 export default function Login({ onLoginUser }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { values, errors, isValid, handleChange } = Validation();
+  const [disabled, setDisabled] = useState(false);
+  const [errorMessage, setErrorMessage,] = useState('');
 
-  function handleEmailChange(e) {
-    setEmail(e.target.value)
+
+  function handleInputsChange(evt) {
+    handleChange(evt);
+    cleanErrorMessage();
   }
 
-  function handlePasswordChange(e) {
-    setPassword(e.target.value)
+  function cleanErrorMessage() {
+    setErrorMessage('');
   }
 
   function handleSubmit(e) {
     e.preventDefault()
-    onLoginUser({ email, password })
+    onLoginUser({
+      email: values.email,
+      password: values.password,
+    })
+    setTimeout(() => {
+      setDisabled(false);
+    }, 2000);
   }
 
   return (
@@ -37,20 +47,25 @@ export default function Login({ onLoginUser }) {
             <input className="login__input"
               placeholder="Email" type="email"
               name="email"
-              value={email || ''}
-              onChange={handleEmailChange}
+              value={values.email || ''}
+              onChange={handleInputsChange}
+              minLength={5} maxLength={30} pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" required
+              disabled={disabled}
             />
+            <span className='auth__error'>{errors.email}</span>
             <span className='login__label'>Пароль</span>
             <input className="login__input"
               placeholder="Пароль" type="password"
               name="password"
-              value={password || ''}
-              onChange={handlePasswordChange}
+              minLength={8} maxLength={30} required disabled={disabled}
+              value={values.password || ''}
+              onChange={handleInputsChange}
             />
-            <span className='auth__error'></span>
+            <span className='auth__error'>{errors.password}</span>
+            <p className={`auth__error-message ${errorMessage && 'auth__error-message_visible'}`}>{errorMessage}</p>
           </div>
           <div className="login__button-container">
-            <button type="submit" className="login__button">Войти</button>
+            <button type="submit" className={`login__button" ${!isValid && 'login__button_disabled'}`}>Войти</button>
             <div className="login__signin">
               <Link to="/signup" className="login__link">
                 Ещё не зарегистрированы? <span className='login__link-signin'>Регистрация</span>
