ebcbfa0f4ebf16d50a2179c5232f5c2b607686ca
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-08-31 20:15:56 +0300

    Тест

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -15,7 +15,7 @@ export default function MoviesCard({ movie, isSaved, onCardSave, onCardDelete, s
 
 
   function onCardClick() {
-    console.log(saved)
+    console.log(isSaved)
     if (saved) {
       onCardDelete(savedMoviesList.filter((m) => m.movieId === movie.id)[0]);
     } else {
