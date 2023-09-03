62e2df193edf9301e44d193217d635d101f6c163
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-09-03 22:58:12 +0300

    Тест

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../FilterCheckbox/FilterCheckbox.css';
 
 export default function FilterCheckbox({ onShortFilmsToggle }) {
   const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem('isShortMoviesChecked');
+    if (storedValue !== null) {
+      setIsChecked(storedValue === 'true');
+    }
+  }, []);
 
   const handleCheckboxChange = () => {
     const updatedValue = !isChecked;
