437aa749ffef3035ae40c26ab3b34eda28a473ef
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-09-03 22:48:21 +0300

    Тест

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -8,35 +8,68 @@ export default function SavedMovies({ movies, savedMoviesList, onCardSave, onCar
   const [filteredMovies, setFilteredMovies] = useState([]);
   const location = useLocation();
   const [isSearchEmpty, setIsSearchEmpty] = useState(false);
+  const [isDurationEmpty, setIsDurationEmpty] = useState(false);
   const [query, setQuery] = useState('');
+  const [isShortMoviesChecked, setIsShortMoviesChecked] = useState(false);
+
+  function filterMovies() {
+    const filteredMovies = movies.filter((movie) => {
+      const lowerCaseQuery = query.toLowerCase();
+      const nameRULowerCase = movie.nameRU.toLowerCase();
+      const nameENLowerCase = movie.nameEN.toLowerCase();
+      return (
+        (nameRULowerCase.includes(lowerCaseQuery) || nameENLowerCase.includes(lowerCaseQuery)) &&
+        (!isShortMoviesChecked || (isShortMoviesChecked && movie.duration <= 40))
+      );
+    });
+    setFilteredMovies(filteredMovies);
+  }
 
   useEffect(() => {
-    let filteredMovies = savedMoviesList;
+    // Восстановление состояния поискового запроса
+    const savedSearchQuery = localStorage.getItem('searchQuery');
+    const initialSearchQuery = savedSearchQuery || '';
+    setQuery(initialSearchQuery);
 
-    if (query) {
-      filteredMovies = filteredMovies.filter((movie) => {
-        const lowerCaseQuery = query.toLowerCase();
-        const nameRULowerCase = movie.nameRU.toLowerCase();
-        const nameENLowerCase = movie.nameEN.toLowerCase();
-        return (
-          (nameRULowerCase.includes(lowerCaseQuery) || nameENLowerCase.includes(lowerCaseQuery)) &&
-          (!movie.duration || (movie.duration && movie.duration <= 40))
-        );
-      });
-    }
+    // Восстановление состояния переключателя короткометражных фильмов
+    const savedIsShortMoviesChecked = localStorage.getItem('isShortMoviesChecked');
+    const initialIsShortMoviesChecked = savedIsShortMoviesChecked === 'true';
+    setIsShortMoviesChecked(initialIsShortMoviesChecked);
 
+    // Фильтрация фильмов в соответствии с текущими фильтрами (поисковый запрос и короткометражные фильмы)
+    const filteredMovies = movies.filter((movie) => {
+      const lowerCaseQuery = initialSearchQuery.toLowerCase();
+      const nameRULowerCase = movie.nameRU.toLowerCase();
+      const nameENLowerCase = movie.nameEN.toLowerCase();
+      return (
+        (nameRULowerCase.includes(lowerCaseQuery) || nameENLowerCase.includes(lowerCaseQuery)) &&
+        (!initialIsShortMoviesChecked || (initialIsShortMoviesChecked && movie.duration <= 40))
+      );
+    });
+
+    // Установка отфильтрованных фильмов в состояние
     setFilteredMovies(filteredMovies);
-    setIsSearchEmpty(filteredMovies.length === 0);
-  }, [savedMoviesList, query]);
+  }, [movies]);
+
+  useEffect(() => {
+    // Сохранение состояния фильтра короткометражных фильмов в localStorage
+    localStorage.setItem('isShortMoviesChecked', isShortMoviesChecked);
+    filterMovies();
+  }, [isShortMoviesChecked]);
 
   function handleSearch(filteredMovies) {
     setFilteredMovies(filteredMovies);
     setIsSearchEmpty(filteredMovies.length === 0);
+    setIsDurationEmpty(
+      filteredMovies.every((movie) => movie.duration > 40)
+    );
   }
 
   function handleQueryChange(newQuery) {
     setQuery(newQuery);
+    localStorage.setItem('searchQuery', newQuery);
   }
+
 
   return (
     <main className="main">
