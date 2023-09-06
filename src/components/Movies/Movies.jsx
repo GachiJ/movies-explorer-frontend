228aa80bf18519786e7c6a5836152a94860d15b5
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-09-06 20:27:10 +0300

    Тест

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function Movies({ movies, savedMoviesList, onCardSave, onCardDelete }) {
-  const [filteredMovies, setFilteredMovies] = useState(filterMovies('', false));
+  const [filteredMovies, setFilteredMovies] = useState(movies);
   const location = useLocation();
   const [isSearchEmpty, setIsSearchEmpty] = useState(false);
   const [isDurationEmpty, setIsDurationEmpty] = useState(true);
